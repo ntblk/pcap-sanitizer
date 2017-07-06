a53fb8a238d84ef7601766e9748445450a04acf2
NetBlocks Team <team@netblocks.org>
2017-07-06 14:06:18 +0000

    Cleanups

--- pcap.js
+++ pcap.js
@@ -1,7 +1,17 @@
 const binstruct = require('binstruct');
-
 const pcap_tables = require('./tables.json');
 
+/*
+typedef struct pcap_hdr_s {
+        guint32 magic_number;   /* magic number
+        guint16 version_major;  /* major version number
+        guint16 version_minor;  /* minor version number
+        gint32  thiszone;       /* GMT to local correction
+        guint32 sigfigs;        /* accuracy of timestamps
+        guint32 snaplen;        /* max length of captured packets, in octets
+        guint32 network;        /* data link type
+} pcap_hdr_t;
+*/
 const pcap_hdr_t = binstruct
 .def({littleEndian:true})
 //.def({littleEndian:false})
@@ -14,6 +24,14 @@ const pcap_hdr_t = binstruct
   .uint32('snaplen')
   .uint32('network');
 
+  /*
+  typedef struct pcaprec_hdr_s {
+          guint32 ts_sec;         /* timestamp seconds
+          guint32 ts_usec;        /* timestamp microseconds
+          guint32 incl_len;       /* number of octets of packet saved in file
+          guint32 orig_len;       /* actual length of packet
+  } pcaprec_hdr_t;
+  */
 const pcaprec_hdr_t = binstruct
   .def({littleEndian:true})
   .uint32('ts_sec')
--- sanitize.js
+++ sanitize.js
@@ -327,17 +327,8 @@ function fixChecksums(layers, frameField) {
 
   if (layers.udp && layers.udp['udp.checksum_raw']) {
     //var dns = parseField(layers.dns_raw, frameField);
-    //var hdr = parseField(layers.udp_raw, frameField);
     var fld = parseField(layers.udp['udp.checksum_raw'], frameField);
-    //var osum = fld.number;
-
-/*
-    const pseudo = Buffer.concat([
-    srcIp, dstIp, new Buffer([0, 17]), length,
-    srcPort, dstPort, length, new Buffer([0, 0]),
-    content
-    ]);
-*/
+    var hdr = parseField(layers.udp_raw, frameField);
 
     function getBuf(layer, name) {
       return parseField(layers[layer][name + '_raw'], frameField).buffer;
@@ -346,40 +337,18 @@ function fixChecksums(layers, frameField) {
     const pseudo = Buffer.concat([
       getBuf('ip', ['ip.src']),
       getBuf('ip', ['ip.dst']),
-      new Buffer([0, 17]),
+      new Buffer([0, 17]), // udp
       getBuf('udp', ['udp.length']),
       getBuf('udp', ['udp.srcport']),
       getBuf('udp', ['udp.dstport']),
       getBuf('udp', ['udp.length']),
       new Buffer([0, 0]),
-      parseField(layers.dns_raw, frameField).buffer,
+      frameField.buffer.slice(hdr.position + hdr.length),
     ]);
 
-    //fld.number = 0;
     fld.number = NetChecksum.raw(pseudo);
   }
 }
-
-/*
-typedef struct pcap_hdr_s {
-        guint32 magic_number;   /* magic number
-        guint16 version_major;  /* major version number
-        guint16 version_minor;  /* minor version number
-        gint32  thiszone;       /* GMT to local correction
-        guint32 sigfigs;        /* accuracy of timestamps
-        guint32 snaplen;        /* max length of captured packets, in octets
-        guint32 network;        /* data link type
-} pcap_hdr_t;
-*/
-
-/*
-typedef struct pcaprec_hdr_s {
-        guint32 ts_sec;         /* timestamp seconds
-        guint32 ts_usec;        /* timestamp microseconds
-        guint32 incl_len;       /* number of octets of packet saved in file
-        guint32 orig_len;       /* actual length of packet
-} pcaprec_hdr_t;
-*/
 
 async function write_pcap (frames, oname) {
 
